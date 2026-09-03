35a1e9d3184a0fff7c205d9da3599b0ab8c78aa0
Hamdyabdelkader <hamdy.kader528@gmail.com>
2026-09-03 19:54:46 +0300

    Add files via upload

---
+++ day-10/index.js
@@ -0,0 +1,6 @@
+import * as math from "./math.module.js";
+ 
+console.log(math.add(5, 3)); 
+console.log(math.subtract(10, 4)); 
+console.log(math.multiply(6, 7));
+console.log(math.divide(20, 4)); 
